18cdcce52b242ffca6838537de6226fb42f65e9e
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-23 20:56:58 +0530

    update

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,23 +1,20 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import dotenv from "dotenv";
-dotenv.config(
-  {path: "../.env"}
-);
+dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 export const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'stock-genius/users',
-    allowed_formats: ['jpg', 'jpeg', 'png'],
-    transformation: [{ width: 500, height: 500, crop: 'limit' }]
-  }
+    folder: "stock-genius/users",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  },
 });
 
 export default cloudinary;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import connectDB from "./config/db.js";
-
 import express, { json } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import authRouter from "./routes/authRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 import stockRouter from "./routes/stockRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
+import watchlistRoutes from "./routes/watchlistRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -27,17 +28,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(helmet());
 }
 
-connectDB();
-
 app.get("/", (req, res) => {
   res.send("Stock Analysis Platform Backend is Running!");
 });
 
-app.use("/api/auth",authRouter);
-app.use("/api/stocks",stockRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/stocks", stockRouter);
+app.use("/api/ai", aiRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+
+// Connect to the database
+connectDB();
 
 app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`
+    `Server running in ${
+      process.env.NODE_ENV || "development"
+    } mode on port http://localhost:${PORT}`
   );
 });
